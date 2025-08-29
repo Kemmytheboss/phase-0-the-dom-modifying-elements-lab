0a8735f6d711ca578fdd4528952efad554e0b302
verah mokaya <verahmokaya475@gmail.com>
2025-08-29 12:53:56 +0300

    DOM assignment done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Write your code here!
-let main = document.querySelector('#main')
-main.remove()
+document.querySelector('main#main').remove();
 
-let newHeader = document.createElement('h1')
-newHeader.id = 'victory'
-newHeader.textContent = 'Enkhbat is the champion'
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Verah Mokaya is the champion'; 
+
+document.body.appendChild(newHeader);
